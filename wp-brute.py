59ab059382dd374b4761c86b489c56c3a65b5ef8
Jay Maranatha Hutajulu <57423203+jayhutajulu1@users.noreply.github.com>
2019-11-16 21:45:23 +0700

    Update wp-brute.py

--- wp-brute.py
+++ wp-brute.py
@@ -2,7 +2,7 @@
 #Ya Maap codingan we berantakan :'v,jangan di recode ya KONTOL.
 
 import requests,readline,re,os,random
-from urllib.Request import urlsplit
+from urllib.request import urlsplit
 requests = requests.Session()
 
 h = '\033[92m'
